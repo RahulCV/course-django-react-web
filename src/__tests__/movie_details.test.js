156b26d2dff371d41407832192bc5fa4e1a9564f
Rahul CV <rahul.cv@walmart.com>
2022-03-05 12:18:27 +0530

    Added test cases

---
+++ src/__tests__/movie_details.test.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {render,fireEvent} from "@testing-library/react";
+import MovieDetails from "../components/movie-details";
+const selectedMovie={
+    id:1,
+    title:"The Shawshank Redemption",
+    description:"Two imprisoned",
+    avg_rating:4,
+    no_of_ratings:"10",
+
+
+}
+describe("SnapShot Test ",()=>{
+    test('movieDtails Should match Snapshot ',()=>{
+       const {container}=render(<MovieDetails movie={selectedMovie} />) 
+       expect(container).toMatchSnapshot(); 
+    })
+
+    test("Should display title and description",()=>{
+        const {queryByText}=render(<MovieDetails movie={selectedMovie} />)
+        expect(queryByText(selectedMovie.title)).toBeTruthy();
+        expect(queryByText(selectedMovie.description)).toBeTruthy();
+
+    })
+    test("rating",()=>{
+        const {container}=render(<MovieDetails movie={selectedMovie} />)
+        const selected_stars = container.querySelectorAll(".orange");
+        expect(selected_stars.length).toBe(selectedMovie.avg_rating);
+    })
+    test("no of Rating",()=>{
+        const {getByTestId}=render(<MovieDetails movie={selectedMovie} />)
+      
+        expect(getByTestId("rating").innerHTML).toBe(selectedMovie.no_of_ratings);
+    })
+
+    test("Mouse over should highlight stars",()=>{
+        const {container}=render(<MovieDetails movie={selectedMovie} />);
+        const stars = container.querySelectorAll(".rate-container svg");
+        stars.forEach((start,index)=>{
+            fireEvent.mouseOver(start);
+            const highlighted_stars = container.querySelectorAll(".purple");
+            expect(highlighted_stars.length).toBe(index+1);
+        })
+
+      
+    })
+
+    test("Mouse leave  should un highlight stars",()=>{
+        const {container}=render(<MovieDetails movie={selectedMovie} />);
+        const stars = container.querySelectorAll(".rate-container svg");
+        stars.forEach((start,index)=>{
+            fireEvent.mouseOver(start);
+            fireEvent.mouseOut(start);
+            const highlighted_stars = container.querySelectorAll(".purple");
+            expect(highlighted_stars.length).toBe(0);
+        })  
+    })
+})
---
+++ src/__tests__/sample.test.js
@@ -0,0 +1,8 @@
+describe('First Suite',()=>{
+    test('simple Test', ()=>{
+        expect(1).toBe(1);
+    });
+    test('simple Test2', ()=>{
+        expect(2).toBe(2);
+    })
+})
